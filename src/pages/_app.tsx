8ffc3ced9c81fefc84628dce845ad7c5af77db0c
williansoncini <willian_soncini@hotmail.com>
2022-06-16 12:14:00 -0300

    configurações iniciais do projeto

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import { AppProps } from "next/app";
+import { GlobalStyle } from "../styles/globalStyle";
+import { ThemeProvider } from "styled-components";
+import { theme } from "../styles/theme";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />;
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
